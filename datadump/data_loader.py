29bcadb15ce5317c32b7e01db79f51f2ffea012c
alinzh <siberiangodness@mail.ru>
2024-08-26 11:31:31 +0000

    Update getting all available data from MPDS

--- datadump/data_loader.py
+++ datadump/data_loader.py
@@ -1,41 +1,19 @@
-import json
 import os.path
 
 import pandas as pd
 from mpds_client import MPDSDataRetrieval
 
 
-class MPDS_Fields:
-    schema = json.load(open("./schema.json"))
-    properties = ["physical properties", "phase diagram"]
-    crystal_structures_fields = schema["definitions"]["crystal_structure"][
-        "properties"
-    ].keys()
-    physical_fields = [
-        "sample.material.chemical_formula",
-        "sample.material.chemical_elements",
-        "sample.material.condition",
-        "sample.material.phase",
-        "sample.material.phase_id",
-        "sample.material.entry",
-        "sample.material.object_repr",
-        "sample.measurement.property",
-        "sample.measurement.condition",
-        "sample.measurement.raw_data",
-    ]
-    diagram_fields = schema["definitions"]["phase_diagram"]["properties"].keys()
-
-
 class DataLoaderMPDS:
     """
-    Make requests to MPDS database, save data in csv format.
+    Make requests to MPDS database, save data in json format
     """
 
     def __init__(self, dtype: int = 7, api_key: str = None) -> None:
         """
         dtype : int
-            Type of receiving data. Available: PEER_REVIEWED = 1, MACHINE_LEARNING = 2,
-            AB_INITIO = 4, ALL = 7
+            Type of receiving data. Available: PEER_REVIEWED = 1,
+            MACHINE_LEARNING = 2, AB_INITIO = 4, ALL = 7
         api_key : str
             Key from MPDS-account
         """
@@ -48,60 +26,74 @@ class DataLoaderMPDS:
         Request atomic structure. Save in dir './data'
         """
         print("---Started receiving: atomic structure---")
+
+        store = None
         for date in range(1965, 2018):
-            if not os.path.isfile(f"./data/atomic_structure_{date}.csv"):
+            if not os.path.isfile(f"./data/atomic_structure.json"):
                 try:
                     dfrm = pd.DataFrame(
                         self.client.get_data(
                             {"props": "atomic structure", "years": str(date)},
-                            fields={"S": MPDS_Fields.crystal_structures_fields},
-                        ),
-                        columns=MPDS_Fields.crystal_structures_fields,
+                            fields={},
+                        )
                     )
-                    dfrm.to_csv(f"./data/atomic_structure_{date}.csv")
-                    print(dfrm)
+                    if not (isinstance(store, pd.DataFrame)):
+                        store = dfrm
+                    else:
+                        store = pd.concat([store, dfrm], ignore_index=True)
                 except Exception as error:
                     print("An exception occurred:", error)
+        store.to_json(f"./data/atomic_structure.json")
+        print("Successfully saved to './data/atomic_structure.json'")
 
     def request_phase_diagram(self):
         """
         Request atomic structure. Save in dir './data'
         """
         print("---Started receiving: phase diagram---")
+
+        store = None
         for date in range(1965, 2018):
-            if not os.path.isfile(f"./data/phase_diagram_{date}.csv"):
+            if not os.path.isfile(f"./data/phase_diagram.json"):
                 try:
                     dfrm = pd.DataFrame(
                         self.client.get_data(
-                            {"props": "phase diagram", "years": str(date)},
-                            fields={"C": MPDS_Fields.diagram_fields},
-                        ),
-                        columns=MPDS_Fields.diagram_fields,
+                            {"props": "phase diagram", "years": str(date)}, fields={}
+                        )
                     )
-                    dfrm.to_csv(f"./data/phase_diagram_{date}.csv")
-                    print(dfrm)
+                    if not (isinstance(store, pd.DataFrame)):
+                        store = dfrm
+                    else:
+                        store = pd.concat([store, dfrm], ignore_index=True)
                 except Exception as error:
                     print("An exception occurred:", error)
+        store.to_json(f"./data/phase_diagram.json")
+        print("Successfully saved to './data/phase_diagram.json'")
 
     def request_phys_properties(self):
         """
         Request physical properties. Save in dir './data'
         """
         print("---Started receiving: physical properties---")
+
+        store = None
         for date in range(1965, 2018):
-            if not os.path.isfile(f"./data/physical_property_{date}.csv"):
+            if not os.path.isfile("./data/physical_property.json"):
                 try:
                     dfrm = pd.DataFrame(
                         self.client.get_data(
                             {"props": "physical properties", "years": str(date)},
-                            fields={"P": MPDS_Fields.physical_fields},
+                            fields={},
                         )
                     )
-                    dfrm.columns = MPDS_Fields.physical_fields
-                    dfrm.to_csv(f"./data/physical_property_{date}.csv")
-                    print(dfrm)
+                    if not (isinstance(store, pd.DataFrame)):
+                        store = dfrm
+                    else:
+                        store = pd.concat([store, dfrm], ignore_index=True)
                 except Exception as error:
                     print("An exception occurred:", error)
+        store.to_json(f"./data/physical_property.json")
+        print("Successfully saved to './data/physical_property.json'")
 
     def request_all_data(self):
         """
@@ -113,5 +105,5 @@ class DataLoaderMPDS:
 
 
 if __name__ == "__main__":
-    client = DataLoaderMPDS(api_key="KEY")
+    client = DataLoaderMPDS(api_key="MPDS_KEY")
     client.request_all_data()
